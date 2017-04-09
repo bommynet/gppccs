6464c45d4d04ac5d0543b8ce3f0f080381035c29
bommynet <mail@bommy.net>
2017-04-09 21:19:00 +0200

    Created progress bar class to show hp/mp on screen.

--- 1bit-clicker-jam/core/src/de/pixlpommes/jam/screens/BattleScreen.java
+++ 1bit-clicker-jam/core/src/de/pixlpommes/jam/screens/BattleScreen.java
@@ -2,9 +2,13 @@ package de.pixlpommes.jam.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import de.pixlpommes.jam.actions.ActionManager;
 import de.pixlpommes.jam.arena.Arena;
+import de.pixlpommes.jam.screens.ui.ProgressBar;
 import de.pixlpommes.jam.units.Player;
 import de.pixlpommes.jam.units.Slime;
 import de.pixlpommes.jam.units.base.Unit;
@@ -17,6 +21,12 @@ import de.pixlpommes.jam.units.base.Unit;
  */
 public class BattleScreen implements Screen {
 
+	/** batch to render everything */
+	private SpriteBatch _batch;
+	
+	/** standard 2D camera */
+	private OrthographicCamera _cam;
+	
 	/** area to place all units */
 	private Arena _arena;
 	
@@ -26,14 +36,24 @@ public class BattleScreen implements Screen {
 	/** flags if fight is currently running (true) or paused/not started (false) */
 	private boolean _isFightRunning;
 	
+	
+	// TODO remove
+	private ProgressBar _pb = new ProgressBar(new String[]{"mana_bar_0.png", "mana_bar_1.png"}, 0, 100, 200, 150);
+	private float val = 1;
+	
+	
 	/**
 	 * Create a battle screen.
 	 */
 	public BattleScreen() {
+		// setup graphics
+		_cam = new OrthographicCamera();
+		_batch = new SpriteBatch();
+		
 		// init
 		_arena = new Arena();
 		_actionManager = new ActionManager(_arena);
-		_isFightRunning = false;
+		_isFightRunning = true; /// TODO start by user input!
 		
 		// create basic units
 		_arena.setPlayer(new Player()); // player
@@ -86,16 +106,23 @@ public class BattleScreen implements Screen {
 	public void render(float delta) {
 		/// Gdx.app.log("BattleScreen", "render called");
 		
+		// clear screen
+        Gdx.gl.glClearColor(0.2f, 0.2f, 0.8f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		// update logic if fight is running
 		if(_isFightRunning)
 			this.update(delta);
-		/// TODO remove!!!
-		else {
-			Gdx.app.log("BattleScreen", "start fight");
-			_isFightRunning = true;
-		}
+		
 		
 		// draw screen always
+		_batch.begin();
+		
+		_pb.updateValue(val);
+		if(_pb.getValue() <= 0f || _pb.getValue() >= 100f) val*=-1;
+		_pb.draw(_batch);
+		
+		_batch.end();
 	}
 	
 	
@@ -114,6 +141,14 @@ public class BattleScreen implements Screen {
 	@Override
 	public void resize(int width, int height) {
 		Gdx.app.log("BattleScreen", "resize called");
+		
+		// update camera
+		_cam.viewportWidth = width;
+		_cam.viewportHeight = height;
+		_cam.update();
+		
+		// update renderer
+		_batch.setProjectionMatrix(_cam.combined);
 	}
 
 	/* (non-Javadoc)
---
+++ 1bit-clicker-jam/core/src/de/pixlpommes/jam/screens/ui/ProgressBar.java
@@ -0,0 +1,87 @@
+package de.pixlpommes.jam.screens.ui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+
+/**
+ * A progress bar build by two custom graphics.
+ * 
+ * @author Thomas Borck - http://www.pixlpommes.de
+ * @version 0.1
+ */
+public class ProgressBar {
+
+	/** image that shows the empty progress bar */
+	private Texture _emptyBar;
+	
+	/** image that shows the filled progress bar */
+	private Texture _filledBar;
+	
+	/** value range for progress */
+	private float _min, _max, _current;
+	
+	/** position of the bar on the screen */
+	private float _x, _y;
+	
+	/**
+	 * Create a new progress bar.
+	 * 
+	 * @param filename name of file for empty and filled bar
+	 * @param min
+	 * @param max
+	 * @param x
+	 * @param y
+	 */
+	public ProgressBar(String[] filename, float min, float max, int x, int y) {
+		_emptyBar = new Texture(Gdx.files.internal(filename[0]));
+		_filledBar = new Texture(Gdx.files.internal(filename[1]));
+		
+		_min = min;
+		_max = max;
+		_current = min;
+		
+		_x = x;
+		_y = y;
+	}
+
+	/**
+	 * Draw progress bar.
+	 * @param batch
+	 */
+	public void draw(Batch batch) {
+		// draw empty bar as base
+		batch.draw(_emptyBar, _x, _y);
+		
+		// draw filled bar partial
+		float normMax = _max - _min;
+		float normCur = _current - _min;
+		float partial = normCur / normMax;
+		batch.draw(_filledBar,
+				_x, _y,
+				_filledBar.getWidth() * partial,
+				(float)_filledBar.getHeight(), 
+				0, 0,
+				(int)(_filledBar.getWidth() * partial),
+				_filledBar.getHeight(),
+				false, false);
+	}
+
+	/**
+	 * Add number to current value.
+	 * @param add
+	 */
+	public void updateValue(float add) {
+		_current += add;
+		
+		_current = _current > _max ? _max : _current;
+		_current = _current < _min ? _min : _current;
+	}
+	
+	/**
+	 * @return
+	 */
+	public float getValue() {
+		return _current;
+	}
+}

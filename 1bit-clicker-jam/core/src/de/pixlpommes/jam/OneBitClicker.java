edf4e08c8fb2fce66669567d2bff93796bd83043
bommynet <mail@bommy.net>
2017-04-09 20:00:46 +0200

    Transfered code from Main to BattleScreen.

--- 1bit-clicker-jam/core/src/de/pixlpommes/jam/OneBitClicker.java
+++ 1bit-clicker-jam/core/src/de/pixlpommes/jam/OneBitClicker.java
@@ -1,33 +1,24 @@
 package de.pixlpommes.jam;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class OneBitClicker extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+import de.pixlpommes.jam.screens.BattleScreen;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+/**
+ * Create game.
+ * 
+ * @author Thomas Borck - http://www.pixlpommes.de
+ * @version 1.0
+ */
+public class OneBitClicker extends Game {
 	
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.ApplicationListener#create()
+	 */
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void create() {
+		Gdx.app.log("Game", "create");
+		setScreen(new BattleScreen());
 	}
 }
---
+++ 1bit-clicker-jam/core/src/de/pixlpommes/jam/screens/BattleScreen.java
@@ -0,0 +1,149 @@
+package de.pixlpommes.jam.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+
+import de.pixlpommes.jam.actions.ActionManager;
+import de.pixlpommes.jam.arena.Arena;
+import de.pixlpommes.jam.units.Player;
+import de.pixlpommes.jam.units.Slime;
+import de.pixlpommes.jam.units.base.Unit;
+
+/**
+ * The main game screen.
+ * 
+ * @author Thomas Borck - http://www.pixlpommes.de
+ * @version 0.1
+ */
+public class BattleScreen implements Screen {
+
+	/** area to place all units */
+	private Arena _arena;
+	
+	/** create, execute, remove all unit actions */
+	private ActionManager _actionManager;
+	
+	/** flags if fight is currently running (true) or paused/not started (false) */
+	private boolean _isFightRunning;
+	
+	/**
+	 * Create a battle screen.
+	 */
+	public BattleScreen() {
+		// init
+		_arena = new Arena();
+		_actionManager = new ActionManager(_arena);
+		_isFightRunning = false;
+		
+		// create basic units
+		_arena.setPlayer(new Player()); // player
+		_arena.setUnit(new Slime(), 2, 1); // party member
+		_arena.setUnit(new Slime(), 3, 1); // enemy
+	}
+	
+	/**
+	 * Update logic.
+	 * 
+	 * @param delta
+	 */
+	public void update(float delta) {
+		// check for inactive party members
+		for(Unit party : _arena.getPartyMembers()) {
+			if(party != null && !party.hasActiveAction()) {
+				// get first enemy and attack it
+				for(int id : Arena.IDS_ENEMY) {
+					if(_arena.getUnit(id) != null) {
+						// attack this unit
+						int x = id % Arena.COLUMNS;
+						int y = (int) Math.floor(id / Arena.COLUMNS);
+						System.out.println(
+								_actionManager.create(party, party.getAbility(0), x, y));
+					}
+				}
+			}
+		}
+		
+		// update action manager
+		_actionManager.doTick(delta);
+		
+		
+		// remove killed units
+		for(int id=0; id<Arena.COLUMNS * Arena.ROWS; id++) {
+			Unit unit = _arena.getUnit(id);
+			
+			if(unit != null && !unit.isAlive()) {
+				// TODO: kill unit
+				System.out.println("kill " + unit);
+				_arena.setUnit(null, id);
+			}
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#show()
+	 */
+	@Override
+	public void show() {
+		Gdx.app.log("BattleScreen", "show called");
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#render(float)
+	 */
+	@Override
+	public void render(float delta) {
+		/// Gdx.app.log("BattleScreen", "render called");
+		
+		// update logic if fight is running
+		if(_isFightRunning)
+			this.update(delta);
+		/// TODO remove!!!
+		else {
+			Gdx.app.log("BattleScreen", "start fight");
+			_isFightRunning = true;
+		}
+		
+		// draw screen always
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#resize(int, int)
+	 */
+	@Override
+	public void resize(int width, int height) {
+		Gdx.app.log("BattleScreen", "resize called");
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#pause()
+	 */
+	@Override
+	public void pause() {
+		Gdx.app.log("BattleScreen", "pause called");
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#resume()
+	 */
+	@Override
+	public void resume() {
+		Gdx.app.log("BattleScreen", "resume called");
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#hide()
+	 */
+	@Override
+	public void hide() {
+		Gdx.app.log("BattleScreen", "hide called");
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#dispose()
+	 */
+	@Override
+	public void dispose() {
+		Gdx.app.log("BattleScreen", "dispose called");
+	}
+
+}

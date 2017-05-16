a4d58effd504d6360043cb048cb5d7125aa907d3
bommynet <mail@bommy.net>
2017-05-16 20:37:49 +0200

    Created tile drafts.

--- core/src/de/pixlpommes/conn3bomb/Arena.java
+++ core/src/de/pixlpommes/conn3bomb/Arena.java
@@ -1,5 +1,9 @@
 package de.pixlpommes.conn3bomb;
 
+import java.util.stream.IntStream;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 
 /**
@@ -16,39 +20,64 @@ import com.badlogic.gdx.graphics.g2d.Batch;
  */
 public class Arena extends ScreenObject {
 
-	// GLOBALS
-	public final static int COLS = 6;
-	public final static int ROWS = 12;
+    // GLOBALS
+    /** TODO: describe COLS */
+    public final static int COLS = 6;
 
-	/**
-	 * 
-	 */
-	public Arena() {
-		this.setOffset(0, 0);
-	}
+    /** TODO: describe ROWS */
+    public final static int ROWS = 12;
+    
+    // GRAPHICS
+    /** TODO: describe TILESIZE */
+    public final static int TILESIZE = 48;
+    
+    /** TODO: describe TILES */
+    public final static Texture TILES = new Texture(Gdx.files.internal("tiles.png"));
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * de.pixlpommes.conn3bomb.ScreenObject#draw(com.badlogic.gdx.graphics.g2d.
-	 * Batch)
-	 */
-	@Override
-	public void draw(Batch batch) {
-		// TODO Auto-generated method stub
+    // GAME
+    /** TODO: describe _tiles */
+    private int[] _tiles;
 
-	}
+    /**
+     * 
+     */
+    public Arena() {
+	this.setOffset(0, 0);
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see de.pixlpommes.conn3bomb.ScreenObject#update(float)
-	 */
-	@Override
-	public void update(float delta) {
-		// TODO Auto-generated method stub
+	// setup game data
+	_tiles = new int[COLS * ROWS];
+	IntStream.range(0, _tiles.length).forEach(index -> _tiles[index] = -1);
+    }
 
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.pixlpommes.conn3bomb.ScreenObject#draw(com.badlogic.gdx.graphics.g2d.
+     * Batch)
+     */
+    @Override
+    public void draw(Batch batch) {
+	// TODO draw background
+	// TODO draw conveyor band
+	// TODO draw blocks/bombs
+	IntStream.range(0, _tiles.length).forEach(index -> {
+	    if(_tiles[index] != -1) {
+		
+	    }
+	});
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.pixlpommes.conn3bomb.ScreenObject#update(float)
+     */
+    @Override
+    public void update(float delta) {
+	// TODO update animation frames -> conveyor band
+	// TODO update block/bomb positions
+	// TODO check destroyables
+    }
 
 }

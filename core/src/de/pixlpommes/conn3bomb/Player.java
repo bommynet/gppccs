d6600c072cff0f63ccf56c50771ef335b5ab13a0
bommynet <mail@bommy.net>
2017-05-22 20:44:10 +0200

    Extended preview for player tiles.

--- core/src/de/pixlpommes/conn3bomb/Player.java
+++ core/src/de/pixlpommes/conn3bomb/Player.java
@@ -1,7 +1,7 @@
 package de.pixlpommes.conn3bomb;
 
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.g2d.Batch;
 
@@ -20,150 +20,163 @@ import com.badlogic.gdx.graphics.g2d.Batch;
  */
 public class Player extends ScreenObject implements InputProcessor {
 
-	/** reference to game arena */
-	private Arena _arena;
+    /** reference to game arena */
+    private Arena _arena;
 
-	/** TODO: describe _pos */
-	private int _pos;
+    /** current column the player stands at */
+    private int _pos;
 
-	/** TODO: describe _blocks */
-	private int _tile;
+    /** TODO: describe _blocks */
+    private int[] _tile;
 
-	/**
-	 * 
-	 */
-	public Player(Arena arena) {
-		this.setOffset(0, 0);
+    /**
+     * 
+     */
+    public Player(Arena arena) {
+        this.setOffset(0, 0);
 
-		_arena = arena;
+        _arena = arena;
 
-		_pos = Arena.COLS / 2;
+        _pos = Arena.COLS / 2;
 
-		_tile = -1;
+        _tile = new int[] {
+                (int) (Math.random() * Tiles.COLORS_COUNT),
+                (int) (Math.random() * Tiles.COLORS_COUNT),
+                (int) (Math.random() * Tiles.COLORS_COUNT)
+        };
 
-		Gdx.input.setInputProcessor(this);
-	}
+        Gdx.input.setInputProcessor(this);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * de.pixlpommes.conn3bomb.ScreenObject#draw(com.badlogic.gdx.graphics.g2d.
-	 * Batch)
-	 */
-	@Override
-	public void draw(Batch batch) {
-		// draw block
-		Tiles.drawBlock(batch, _tile, _offsetX + _pos * Tiles.TILESIZE,
-				_offsetY);
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.pixlpommes.conn3bomb.ScreenObject#draw(com.badlogic.gdx.graphics.g2d.
+     * Batch)
+     */
+    @Override
+    public void draw(Batch batch) {
+        // draw blocks
+        Tiles.drawBlock(batch, _tile[0], _offsetX + _pos * Tiles.TILESIZE,
+                _offsetY);
+        Tiles.drawBlockThumbnail(batch, _tile[1],
+                _offsetX + (_pos + 1) * Tiles.TILESIZE,
+                _offsetY - 0.25f * Tiles.TILESIZE);
+        Tiles.drawBlockThumbnail(batch, _tile[2],
+                _offsetX + (_pos + 1.5f) * Tiles.TILESIZE,
+                _offsetY - 0.25f * Tiles.TILESIZE);
 
-		// TODO: draw player
-		Tiles.drawPlayer(batch, _offsetX + _pos * Tiles.TILESIZE, _offsetY);
-	}
+        // draw player
+        Tiles.drawPlayer(batch, _offsetX + _pos * Tiles.TILESIZE, _offsetY);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see de.pixlpommes.conn3bomb.ScreenObject#update(float)
-	 */
-	@Override
-	public void update(float delta) {
-		if (_tile < 0)
-			_tile = (int) (Math.random() * 3);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.pixlpommes.conn3bomb.ScreenObject#update(float)
+     */
+    @Override
+    public void update(float delta) {
+        
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.badlogic.gdx.InputProcessor#keyDown(int)
-	 */
-	@Override
-	public boolean keyDown(int keycode) {
-		if (keycode == Keys.A) {
-			_pos--;
-			if (_pos < 0)
-				_pos = 0;
-		} else if (keycode == Keys.D) {
-			_pos++;
-			if (_pos >= Arena.COLS)
-				_pos = Arena.COLS - 1;
-		} else if (keycode == Keys.SPACE) {
-			_arena.addBottom(_pos, _tile);
-			_tile = -1;
-		}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.badlogic.gdx.InputProcessor#keyDown(int)
+     */
+    @Override
+    public boolean keyDown(int keycode) {
+        if (keycode == Keys.A) {
+            _pos--;
+            if (_pos < 0)
+                _pos = 0;
+        }
+        else if (keycode == Keys.D) {
+            _pos++;
+            if (_pos >= Arena.COLS)
+                _pos = Arena.COLS - 1;
+        }
+        else if (keycode == Keys.SPACE) {
+            _arena.addBottom(_pos, _tile[0]);
+            _tile[0] = _tile[1];
+            _tile[1] = _tile[2];
+            _tile[2] = (int) (Math.random() * Tiles.COLORS_COUNT);
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.badlogic.gdx.InputProcessor#keyUp(int)
-	 */
-	@Override
-	public boolean keyUp(int keycode) {
-		return false;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.badlogic.gdx.InputProcessor#keyUp(int)
+     */
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.badlogic.gdx.InputProcessor#keyTyped(char)
-	 */
-	@Override
-	public boolean keyTyped(char character) {
-		return false;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.badlogic.gdx.InputProcessor#keyTyped(char)
+     */
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.badlogic.gdx.InputProcessor#touchDown(int, int, int, int)
-	 */
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer,
-			int button) {
-		return false;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.badlogic.gdx.InputProcessor#touchDown(int, int, int, int)
+     */
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer,
+            int button) {
+        return false;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.badlogic.gdx.InputProcessor#touchUp(int, int, int, int)
-	 */
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.badlogic.gdx.InputProcessor#touchUp(int, int, int, int)
+     */
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.badlogic.gdx.InputProcessor#touchDragged(int, int, int)
-	 */
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		return false;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.badlogic.gdx.InputProcessor#touchDragged(int, int, int)
+     */
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.badlogic.gdx.InputProcessor#mouseMoved(int, int)
-	 */
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		return false;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.badlogic.gdx.InputProcessor#mouseMoved(int, int)
+     */
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.badlogic.gdx.InputProcessor#scrolled(int)
-	 */
-	@Override
-	public boolean scrolled(int amount) {
-		return false;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.badlogic.gdx.InputProcessor#scrolled(int)
+     */
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 
 }
--- core/src/de/pixlpommes/conn3bomb/Tiles.java
+++ core/src/de/pixlpommes/conn3bomb/Tiles.java
@@ -17,56 +17,82 @@ import com.badlogic.gdx.graphics.g2d.Batch;
  */
 public class Tiles {
 
-	/** TODO: describe TILESIZE */
-	public final static int TILESIZE = 48;
+    /** TODO: describe TILESIZE */
+    public final static int TILESIZE = 48;
 
-	/** tile set as single file */
-	public final static Texture TILES = new Texture(
-			Gdx.files.internal("tiles.png"));
+    /** TODO: describe COLORS_COUNT */
+    public final static int COLORS_COUNT = 3;
 
-	/**
-	 * @param batch
-	 * @param x
-	 * @param y
-	 */
-	public static void drawConvoyer(Batch batch, float x, float y) {
-		batch.draw(TILES, // tile set file
-				x, y, // position on screen
-				0, 2 * Tiles.TILESIZE, // tile position in file
-				Tiles.TILESIZE, Tiles.TILESIZE // tile size
-		);
-	}
+    /** tile set as single file */
+    public final static Texture TILES = new Texture(
+            Gdx.files.internal("tiles.png"));
 
-	/**
-	 * @param batch
-	 * @param id
-	 * @param x
-	 * @param y
-	 */
-	public static void drawBlock(Batch batch, int id, float x, float y) {
-		// tile id == -1 -> invisible, so don't draw it
-		if (id == -1)
-			return;
+    /**
+     * @param batch
+     * @param x
+     * @param y
+     */
+    public static void drawConvoyer(Batch batch, float x, float y) {
+        batch.draw(TILES, // tile set file
+                x, y, // position on screen
+                0, 2 * Tiles.TILESIZE, // tile position in file
+                Tiles.TILESIZE, Tiles.TILESIZE // tile size
+        );
+    }
 
-		int tilePosX = (id + 1) * Tiles.TILESIZE;
+    /**
+     * @param batch
+     * @param id
+     * @param x
+     * @param y
+     */
+    public static void drawBlock(Batch batch, int id, float x, float y) {
+        // tile id == -1 -> invisible, so don't draw it
+        if (id == -1)
+            return;
 
-		batch.draw(TILES, // tile set file
-				x, y, // position on screen
-				tilePosX, Tiles.TILESIZE, // tile position in file
-				Tiles.TILESIZE, Tiles.TILESIZE// tile size
-		);
-	}
+        int tilePosX = (id + 1) * Tiles.TILESIZE;
 
-	/**
-	 * @param batch
-	 * @param x
-	 * @param y
-	 */
-	public static void drawPlayer(Batch batch, float x, float y) {
-		batch.draw(TILES, // tile set file
-				x, y, // position on screen
-				3 * Tiles.TILESIZE, 2 * Tiles.TILESIZE, // tile position in file
-				TILESIZE, TILESIZE// tile size
-		);
-	}
+        batch.draw(TILES, // tile set file
+                x, y, // position on screen
+                tilePosX, Tiles.TILESIZE, // tile position in file
+                Tiles.TILESIZE, Tiles.TILESIZE// tile size
+        );
+    }
+
+    /**
+     * @param batch
+     * @param id
+     * @param x
+     * @param y
+     */
+    public static void drawBlockThumbnail(Batch batch, int id, float x,
+            float y) {
+        // tile id == -1 -> invisible, so don't draw it
+        if (id == -1)
+            return;
+
+        int tilePosX = (id + 1) * Tiles.TILESIZE;
+
+        batch.draw(TILES, // tile set file
+                x, y, // position on screen
+                TILESIZE / 2, TILESIZE / 2, // drawing tile sized
+                tilePosX, Tiles.TILESIZE, // tile position in file
+                Tiles.TILESIZE, Tiles.TILESIZE, // tile size
+                false, false // mirroring
+        );
+    }
+
+    /**
+     * @param batch
+     * @param x
+     * @param y
+     */
+    public static void drawPlayer(Batch batch, float x, float y) {
+        batch.draw(TILES, // tile set file
+                x, y, // position on screen
+                3 * Tiles.TILESIZE, 2 * Tiles.TILESIZE, // tile position in file
+                TILESIZE, TILESIZE// tile size
+        );
+    }
 }
